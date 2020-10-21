5d8efe549bff190b44b79288493cd9c691d0201c
Wilder León <wilder_eduardo85@hotmail.com>
2020-10-21 09:18:38 -0300

    Se agregó validación a la barra de búsqueda y se eliminó estilos css innecesarios

--- main.js
+++ main.js
@@ -5,21 +5,28 @@ $(document).ready(()=>{
     
         $('#formulario').submit((e)=>{
             e.preventDefault();
-            let busqueda = $('#busqueda').val();
+            const busqueda = $('#busqueda').val();
 
-            let resultado = clientes.filter((item)=>{
+            const resultado = clientes.filter((item)=>{
                 return (item.rut === busqueda);
             });
 
-            $('#nombres').text(resultado[0].nombres +' '+ resultado[0].apellidos);
-            $('#imagen').attr('src', resultado[0].foto);
-            $('#distribuidor').text(resultado[0].distribuidor);
-            $('#departamento').text(resultado[0].departamento);
-            $('#oficina').text(resultado[0].oficina);
-            $('#email').text(resultado[0].email);
-            $('#telefono').text(resultado[0].telefono);
-            $('#fotoArticulo').attr('src', resultado[0].fotoArticulo);
-           
+            if (Object.keys(resultado).length>=1){
+
+                $('#ingreseRut').html('Ingrese Rut');
+                $('#nombres').text(resultado[0].nombres +' '+ resultado[0].apellidos);
+                $('#imagen').attr('src', resultado[0].foto);
+                $('#distribuidor').text(resultado[0].distribuidor);
+                $('#departamento').text(resultado[0].departamento);
+                $('#oficina').text(resultado[0].oficina);
+                $('#email').text(resultado[0].email);
+                $('#telefono').text(resultado[0].telefono);
+                $('#fotoArticulo').attr('src', resultado[0].fotoArticulo);
+
+            }else{
+                $('#ingreseRut').html(`<p style="color: rgb(255, 255, 149)">
+                <i class="fas fa-exclamation-triangle"></i> El rut ingresado no es válido!!</p>`);
+            }
 
         })
         
